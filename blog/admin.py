d2714157fe07e2f0767c359ecdb9092860128e23
siswanto <siswanto214@yahoo.com>
2019-10-11 00:30:05 +0700

    Changed the HTML for the site

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Post
 
-# Register your models here.
+# Register your models here.9728ffd5ba74a31456fc84f2703718d67e878fb9
 admin.site.register(Post)
